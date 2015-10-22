9d5dbf2077228fdc22046ddbf7997dc1a2050548
shuhei <kawashu0723@gmail.com>
2015-10-23 02:01:17 +0900

    fix output format

--- wc/wc.rb
+++ wc/wc.rb
@@ -2,56 +2,84 @@ require 'optparse'
 
 opt = OptionParser.new
 OPTS = {}
-
-opt.on('-w') {|v| OPTS[:w] = v }
-opt.on('-l') {|v| OPTS[:l] = v }
-opt.on('-c') {|v| OPTS[:c] = v }
-
+opt.on('-w') { |v| OPTS[:w] = v }
+opt.on('-l') { |v| OPTS[:l] = v }
+opt.on('-c') { |v| OPTS[:c] = v }
 opt.parse!(ARGV)
-# p ARGV
-# p OPTS
 
-tWordNum = 0
-tLineNum = 0
-tCharNum = 0
+word_array = []
+line_array = []
+char_array = []
 
 ARGV.each do |f|
   file = open(f)
 
-  wNum = 0
-  lNum = 0
-  cNum = 0
+  w_num = 0
+  l_num = 0
+  c_num = 0
 
   file.each do |line|
-    lNum += 1
-    cNum += line.size  # wcは改行文字も含んだバイト数を出力するので取り除く前に
-    line.chomp!           # 改行を取り除く
-    words = line.split(/\s+/).reject{|w| w.empty?}  # スペースでsplit、行頭がスペースの場合wordsに入ってしまうので取り除く
-    wNum += words.size
+    l_num += 1
+    c_num += line.size
+    line.chomp! # remove LF
+    words = line.split(/\s+/).reject(&:empty?) # split space
+    w_num += words.size
   end
 
-  if OPTS.empty?
-    puts "#{lNum} #{wNum} #{cNum} #{f}"
-  else
-    OPTS.each{|key, value|
-      if key == :l && value == true
-        print "#{lNum} "
-      elsif key == :w && value == true
-        print  "#{wNum} "
-      elsif key == :c && value == true
-        print "#{cNum} "
-      end
-    }
-    puts f
-  end
+  word_array.push(w_num)
+  line_array.push(l_num)
+  char_array.push(c_num)
 
-  tLineNum += lNum
-  tWordNum += wNum
-  tCharNum += cNum
-  
   file.close
 end
 
+def sum(ary)
+  total = 0
+  ary.each do |n|
+    total += n
+  end
+  total
+end
+
+total_line = sum(line_array)
+total_word = sum(word_array)
+total_char = sum(char_array)
+digit = [total_line, total_word, total_char].max.to_s.length + 2
+
+ARGV.count.times do |n|
+  if OPTS.empty?
+    puts "#{format('%*d', digit, line_array[n])} \
+    #{format('%*d', digit, word_array[n])} \
+    #{format('%*d', digit, char_array[n])} " + ARGV[n]
+  else
+    OPTS.each do|key, value|
+      if key == :l && value == true
+        print format('%*d', digit, line_array[n])
+      elsif key == :w && value == true
+        print format('%*d', digit, word_array[n])
+      elsif key == :c && value == true
+        print format('%*d', digit, char_array[n])
+      end
+    end
+    puts " #{ARGV[n]}"
+  end
+end
+
 if ARGV.count > 1
-  puts "#{tLineNum} #{tWordNum} #{tCharNum} total"
+  if OPTS.empty?
+    puts "#{format('%*d', digit, total_line)} \
+    #{format('%*d', digit, total_word)} \
+    #{format('%*d', digit, total_char)} " + 'total'
+  else
+    OPTS.each do|key, value|
+      if key == :l && value == true
+        print format('%*d', digit, total_line)
+      elsif key == :w && value == true
+        print format('%*d', digit, total_word)
+      elsif key == :c && value == true
+        print format('%*d', digit, total_char)
+      end
+    end
+    puts ' total'
+  end
 end
