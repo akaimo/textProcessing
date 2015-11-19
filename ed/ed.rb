658af898d42f2b49277d48acb5afd850cdb6f835
shuhei <kawashu0723@gmail.com>
2015-11-19 14:51:55 +0900

    complete 'a' command

--- ed/ed.rb
+++ ed/ed.rb
@@ -22,20 +22,22 @@ class REPL
     cmnd = '(?:wq|[acdefgijkmnpqrsw=]|\s|\z)'
     prmt = '(?:.*)'
     @cmd = @cmd.match(/\A(?:(#{addr})(?:,(#{addr}))?)?(#{cmnd})(#{prmt})?\z/)
-    p @cmd # debug
+    # p @cmd # debug
 
+    @output = true
     if @cmd.nil?
       @result = '?'
     else
       # execution command
       @result = '?'
       newline if @cmd[3] == ''
+      add if @cmd[3] == 'a'
       exit if @cmd[3] == 'q'
     end
   end
 
   def print
-    puts @result
+    puts @result if @output == true
     puts "now_line: #{@current_line}" # debug
   end
 
@@ -64,6 +66,26 @@ class REPL
       @current_line = @cmd[1].to_i
       @result = @buffer[@current_line - 1]
     end
+  end
+
+  def add
+    return unless @cmd[4] == ''
+
+    @new_buffer = []
+    loop do
+      str = STDIN.gets
+      break if str == ".\n"
+      @new_buffer << str
+    end
+
+    @current_line = @cmd[1].to_i unless @cmd[1].nil?
+
+    @new_buffer.each do |str|
+      @buffer.insert(@current_line, str)
+      @current_line += 1
+    end
+
+    @output = false
   end
 end
 
